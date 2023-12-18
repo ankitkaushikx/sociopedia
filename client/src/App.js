96361ddaba9fa42ecd6d45b8c52fd03b2a05a617
Ankit Kaushik <ankitkaushikx@gmail.com>
2023-12-18 16:31:07 +0530

    Client Redux , theme, state setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => {
+    return state.mode;
+  });
+  const theme = useMemo(() => {
+    return createTheme(themeSettings(mode));
+  }, [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
